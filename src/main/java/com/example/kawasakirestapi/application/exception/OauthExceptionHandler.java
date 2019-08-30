3d0587600fdd9864d2912b76b94363a4d89aee42
川崎 亮輔 <kawasakiryosuke@team-lab.com>
2019-08-30 18:21:45 +0900

    [update]・例外クラスの処理をhandlerに追加

--- src/main/java/com/example/kawasakirestapi/application/exception/OauthExceptionHandler.java
+++ src/main/java/com/example/kawasakirestapi/application/exception/OauthExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.example.kawasakirestapi.application.exception;
 
+import com.example.kawasakirestapi.application.exception.oauth.InvalidAuthorizeException;
+import com.example.kawasakirestapi.application.exception.oauth.TokenNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -24,6 +30,25 @@ public class OauthExceptionHandler extends ResponseEntityExceptionHandler {
     public String handleInvalidAuthorizeException(InvalidAuthorizeException ex) {
         log.warn(ex.getMessage(), ex);
         return "/error/401";
+    }
+
+    /**
+     * リクエストにtokenが存在しない場合にエラーを返す
+     *
+     * @param ex      例外
+     * @param request リクエスト
+     * @return 401エラー情報を返す
+     */
+    @ExceptionHandler({ TokenNotFoundException.class })
+    public ResponseEntity<Object> tokenNotFoundException(
+            TokenNotFoundException ex, WebRequest request) {
+
+        log.warn(ex.getMessage(), ex);
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponse body = new ErrorResponse(status, ex.getMessage());
+
+        return handleExceptionInternal(ex, body, headers, status, request);
     }
 
 
