a4d20295ccab7c6817c1c9f0d3147fc25be2d5e9
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-07-11 16:19:14 +0900

    [change]・NotFoundException追記

--- src/main/java/com/example/kawasakirestapi/application/exception/Item/NotFoundException.java
+++ src/main/java/com/example/kawasakirestapi/application/exception/Item/NotFoundException.java
@@ -1,4 +1,9 @@
 package com.example.kawasakirestapi.application.exception.Item;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException{
+    private static final long serialVersionUID = -2631315510426530295L;
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
 }
