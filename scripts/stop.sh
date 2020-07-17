0fbf9e11cddab7eb135c80cac4bd16df99c2f639
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-07-17 12:35:20 +0900

    [fix]・コメントアウト

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-
-PID=$(ps -ax | grep kawasaki-restapi.jar | grep -v grep | awk '{print $1}')
-kill ${PID}
+##!/usr/bin/env bash
+#
+#PID=$(ps -ax | grep kawasaki-restapi.jar | grep -v grep | awk '{print $1}')
+#kill ${PID}
