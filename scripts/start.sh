52c32687f13ab719b10ec7c8ee6620d9eb354aed
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-07-17 12:49:45 +0900

    no message

--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-##!/usr/bin/env bash
-#source ~/.bash_profile
-#nohup java \
-# -Dspring.profiles.active=production
-# -jar /var/app/kawasaki-restapi.jar >> /var/log/kawasaki-restapi/nohup.out 2>&1 < /dev/null &
--- scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-##!/usr/bin/env bash
-#
-#PID=$(ps -ax | grep kawasaki-restapi.jar | grep -v grep | awk '{print $1}')
-#kill ${PID}
