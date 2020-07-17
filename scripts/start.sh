ed11844af62c426153f66ede4cec439244461330
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-07-17 19:50:08 +0900

    [fix]・軽微修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 ##!/usr/bin/env bash
 source ~/.bash_profile
 nohup java \
- -Dspring.profiles.active=production
+ -Dspring.profiles.active=production \
  -jar /var/app/kawasaki-restapi.jar >> /var/log/kawasaki-restapi/nohup.out 2>&1 < /dev/null &
