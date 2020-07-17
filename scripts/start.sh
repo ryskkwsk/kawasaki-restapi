be375f3ad21f285d931d6f2f3f2f7d7daae00302
RyosukeKawasaki <kawasakiryosuke@team-lab.com>
2020-07-17 19:56:20 +0900

    [fix]・スクリプト修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 source ~/.bash_profile
 nohup java \
  -Dspring.profiles.active=production \
- -jar /var/app/kawasaki-restapi.jar >> /var/log/kawasaki-restapi/nohup.out 2>&1 < /dev/null &
+ -jar /home/ec2-user/kawasaki-restapi/kawasaki-restapi-0.0.1-SNAPSHOT.jar >> /var/log/kawasaki-restapi/nohup.out 2>&1 < /dev/null &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PID=$(ps -ax | grep kawasaki-restapi.jar | grep -v grep | awk '{print $1}')
+PID=$(ps -ax | grep kawasaki-restapi-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $1}')
 if [ -n "${PID}" ]; then
   sudo kill -9 ${PID}
 fi
